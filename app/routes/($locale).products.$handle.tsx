fdffbcbeb61e66a1b0c832958b0c402e236a5ece
Sivagamisundari <sivagamisundari.k@particleblack.com>
2026-05-05 10:47:48 +0530

    redeployment

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -510,12 +510,14 @@ export default function Product() {
   return (
     <div className='bg-[#fdfaf5] 2xl:px-[5rem] lg:px-[4rem]'>
       <p className='text-[14px] text-gray-400 px-6 pt-4 font-sans'><a href='/'> Home</a> | <span className='text-black'>{product.title}</span></p>
+      
       {reviewSuccessBanner ? (
         <div className="mx-6 mt-4 flex items-center gap-3 bg-[#e2ede2] px-4 py-3 text-[#14813f]">
           <span className="text-lg leading-none">✓</span>
           <span className="font-sans text-base">{reviewSuccessBanner}</span>
         </div>
       ) : null}
+
       <div className="mx-auto px-6 py-10 w-full flex flex-col lg:flex-row lg:gap-[5rem] items-center">
         
         {/* LEFT SIDE */}
@@ -949,6 +951,7 @@ export default function Product() {
           }}
         />
       </div>
+
       {/* PRODUCT DETAILS + REVIEW SECTION */}
       <div className="bg-white rounded-xl p-6 space-y-6">
         <div className='grid lg:grid-cols-5  gap-16'>
@@ -1172,6 +1175,8 @@ export default function Product() {
         </div>
 
       </div>
+
+
       {/* New Section */}
       <section className="lg:mt-8 2xl:px-[5rem] lg:px-[4rem] lg:py-48 lg:mb-0 mb-6 py-5 mt-0 bg-white">
         <div className="mx-auto relative">
@@ -1219,6 +1224,7 @@ export default function Product() {
           </div>
         </div>
       </section>
+
       {isOpen && (
         <ImageModal
           images={images}
@@ -1227,6 +1233,7 @@ export default function Product() {
           onClose={() => setIsOpen(false)}
         />
       )}
+
       {isCallbackOpen && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 px-4"
@@ -1318,6 +1325,7 @@ export default function Product() {
           </div>
         </div>
       )}
+
       {isVideoCallOpen && (
         <div
           className="fixed inset-0 z-50 flex items-start pt-[4rem] justify-center bg-black/50 px-4"
@@ -1440,6 +1448,7 @@ export default function Product() {
           </div>
         </div>
       )}
+      
       {isDiamondCertificateGuideOpen && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 px-4"
